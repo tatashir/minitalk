4c14eb8c9b3d2b15598e21b7dd64d7d1c8788821
taichi tashiro <taichi@taichinoMacBook-Air.local>
2023-04-03 21:51:04 +0900

    binary-naosita

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: tatashir <tatashir@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/03 14:33:39 by tatashir          #+#    #+#             */
-/*   Updated: 2023/04/03 15:09:34 by tatashir         ###   ########.fr       */
+/*   Updated: 2023/04/03 21:50:48 by tatashir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ char ft_conversion_bit_10(int a_2)
 {
     int rtn;
     int num;
+    rtn = 0;
+    num = 1;
     while(0 < a_2)
     {
         rtn = rtn + ((a_2 % 10) * num);
@@ -25,3 +27,12 @@ char ft_conversion_bit_10(int a_2)
     return ((char)rtn);
 }
 
+void    binary_converter(int num)
+{
+    static int r_binary_n;
+    static int count;
+    char r_put;
+
+    if (num == SIGUSR1)
+        r_binary_n = (r)
+}
