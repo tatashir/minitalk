d40ade609f47aacb8747905442754fa9049b580b
Taichi Tashiro <tatashir@c1r11s7.42tokyo.jp>
2023-04-05 19:58:47 +0900

    norminetteisok

--- client.c
+++ client.c
@@ -6,82 +6,82 @@
 /*   By: tatashir <tatashir@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/26 18:13:49 by tatashir          #+#    #+#             */
-/*   Updated: 2023/04/05 16:40:14 by tatashir         ###   ########.fr       */
+/*   Updated: 2023/04/05 19:54:02 by tatashir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-int id_check(char *argv)
+int	id_check(char *argv)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (argv[i] != '\0')
-    {
-        if (!ft_isdigit(argv[i]))
-            return (1);
-        i++;
-    }
-    return (0);
+	i = 0;
+	while (argv[i] != '\0')
+	{
+		if (!ft_isdigit(argv[i]))
+			return (1);
+		i++;
+	}
+	return (0);
 }
 
-int arguments_check(int argc, char **argv)
+int	arguments_check(int argc, char **argv)
 {
-    int n;
+	int	n;
 
-    if (argc != 3)
-        return (1);
-    if (id_check(argv[1]))
-        return (1);
-    n = ft_atoi(argv[1]);
-    if (n < 0 || 99999 < n)
-        return (1);
-    return (0);
+	if (argc != 3)
+		return (1);
+	if (id_check(argv[1]))
+		return (1);
+	n = ft_atoi(argv[1]);
+	if (n < 0 || 99999 < n)
+		return (1);
+	return (0);
 }
 
-static void conversion_2bit(int n, char str[9])
+static void	conversion_2bit(int n, char str[9])
 {
-    int i;
-    int j;
-    int rbase;
+	int	i;
+	int	j;
+	int	rbase;
 
-    i = 0;
-    j = 7;
-    while (0 <= j)
-    {
-        rbase = 1 << i;
-        rbase = n & rbase;
-        str[j] = (rbase >> i) + '0';
-        i++;
-        j--;
-    }
+	i = 0;
+	j = 7;
+	while (0 <= j)
+	{
+		rbase = 1 << i;
+		rbase = n & rbase;
+		str[j] = (rbase >> i) + '0';
+		i++;
+		j--;
+	}
 }
 
-int main(int a_argc, char *a_argv[])
+int	main(int a_argc, char *a_argv[])
 {
-    int i;
-    int j;
-    char r_str[9];
+	int		i;
+	int		j;
+	char	r_str[9];
 
-    if (arguments_check(a_argc, a_argv))
-        return (0);
-    i = 0;
-    r_str[8] = '\0';
-    while (a_argv[2][i] != '\0')
-    {
-        conversion_2bit((int)a_argv[2][i], r_str);
-        j = 0;
-        while (j < 8)
-        {
-            if (r_str[j] == '1')
-                kill((pid_t)ft_atoi(a_argv[1]), SIGUSR1);
-            else if (r_str[j] == '0')
-                kill ((pid_t)ft_atoi(a_argv[1]), SIGUSR2);
-            j++;
-            usleep(450);
-        }
-        i++;
-    }
-    return (0);
+	if (arguments_check(a_argc, a_argv))
+		return (0);
+	i = 0;
+	r_str[8] = '\0';
+	while (a_argv[2][i] != '\0')
+	{
+		conversion_2bit((int)a_argv[2][i], r_str);
+		j = 0;
+		while (j < 8)
+		{
+			if (r_str[j] == '1')
+				kill((pid_t)ft_atoi(a_argv[1]), SIGUSR1);
+			else if (r_str[j] == '0')
+				kill ((pid_t)ft_atoi(a_argv[1]), SIGUSR2);
+			j++;
+			usleep(450);
+		}
+		i++;
+	}
+	return (0);
 }
--- server.c
+++ server.c
@@ -6,62 +6,63 @@
 /*   By: tatashir <tatashir@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/03 14:33:39 by tatashir          #+#    #+#             */
-/*   Updated: 2023/04/05 16:33:54 by tatashir         ###   ########.fr       */
+/*   Updated: 2023/04/05 19:57:22 by tatashir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-char conversion_10bit(int a_2)
+char	conversion_10bit(int a_2)
 {
-    int rtn;
-    int num;
-    rtn = 0;
-    num = 1;
-    while(0 < a_2)
-    {
-        rtn = rtn + ((a_2 % 10) * num);
-        a_2 = a_2 /10;
-        num *= 2;
-    }
-    return ((char)rtn);
+	int	rtn;
+	int	num;
+
+	rtn = 0;
+	num = 1;
+	while (0 < a_2)
+	{
+		rtn = rtn + ((a_2 % 10) * num);
+		a_2 = a_2 / 10;
+		num *= 2;
+	}
+	return ((char)rtn);
 }
 
-void    binary_converter(int num)
+void	binary_converter(int num)
 {
-    static int r_binary_n;
-    static int count;
-    char r_put;
+	static int	r_binary_n;
+	static int	count;
+	char		r_put;
 
-    if (num == SIGUSR1)
-        r_binary_n = (r_binary_n * 10) + 1;
-    if (num == SIGUSR2)
-        r_binary_n = (r_binary_n * 10) + 0;
-    count++;
-    if (count == 8)
-    {
-        r_put = conversion_10bit(r_binary_n);
-        write(1, &r_put, 1);
-        r_binary_n = 0;
-        count = 0;
-        r_put = '\0';
-    }
+	if (num == SIGUSR1)
+		r_binary_n = (r_binary_n * 10) + 1;
+	if (num == SIGUSR2)
+		r_binary_n = (r_binary_n * 10) + 0;
+	count++;
+	if (count == 8)
+	{
+		r_put = conversion_10bit(r_binary_n);
+		write(1, &r_put, 1);
+		r_binary_n = 0;
+		count = 0;
+		r_put = '\0';
+	}
 }
 
-int main(void)
+int	main(void)
 {
-    struct sigaction r_sa;
+	struct sigaction	r_sa;
 
-    ft_bzero(&r_sa, sizeof(struct sigaction));
-    r_sa.sa_handler = binary_converter;
-    sigemptyset(&r_sa.sa_mask);
-    sigaddset(&r_sa.sa_mask, SIGUSR1);
-    sigaddset(&r_sa.sa_mask, SIGUSR2);
-    r_sa.sa_flags = 0;
-    ft_printf("%d\n", getpid());
-    sigaction(SIGUSR1, &r_sa, NULL);
-    sigaction(SIGUSR2, &r_sa, NULL);
-    while (1)
-        pause();
-    return (0);
+	ft_bzero(&r_sa, sizeof(struct sigaction));
+	r_sa.sa_handler = binary_converter;
+	sigemptyset(&r_sa.sa_mask);
+	sigaddset(&r_sa.sa_mask, SIGUSR1);
+	sigaddset(&r_sa.sa_mask, SIGUSR2);
+	r_sa.sa_flags = 0;
+	ft_printf("%d\n", getpid());
+	sigaction(SIGUSR1, &r_sa, NULL);
+	sigaction(SIGUSR2, &r_sa, NULL);
+	while (1)
+		pause();
+	return (0);
 }
